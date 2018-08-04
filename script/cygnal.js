a9e30c786ce7cf5933c2aa445f67a2da281c259a
rachionerebir <rachionerebir@gmail.com>
2018-08-05 05:31:22 +0800

    initial commit

--- script/cygnal.js
+++ script/cygnal.js
@@ -4,34 +4,158 @@
     let ImgFactory = {};
     let ObjectData = {};
     let StageData = {};
+    let CharaData = {};
 
     let gameCtx;
     let renderTick;
 
-    let GameManeger = function () {
+    let GameManager = function () {
+        let BodyManager = {
+            TYPE: {
+                CHARA: 0,
+                CUBE: 1
+            },
+            objects: [],
+            insertNewBody: function (type, position, posErr, collisionStatus) {
+                let obj = {};
+                obj.type = type;
+                obj.pos = position;
+                obj.posErr = posErr;
+                obj.collisionStatus = collisionStatus;
+                obj.id = this.objects.length;
+                this.objects.push(obj);
+            },
+            getRealBody: function (obj) {
+                return {
+                    x: obj.pos.x + obj.posErr.x,
+                    y: obj.pos.y + obj.posErr.y,
+                    width: obj.posErr.width,
+                    height: obj.posErr.height
+
+                }
+            },
+            isMovableType: function (obj) {
+                return obj.collisionStatus.canfall;
+            },
+            setIsGround: function (obj, isground) {
+                if (this.isMovableType(obj) && !obj.collisionStatus.isground) {
+                    obj.collisionStatus.velocity.y = 0;
+                    obj.collisionStatus.isground = true;
+                }
+
+            },
+            setIsFly: function (obj) {
+                if (this.isMovableType(obj) && obj.collisionStatus.isground) {
+                    obj.collisionStatus.isground = false;
+                }
+            },
+            isOverlap_rect: function (r1, r2) {
+                let r1l = r1.x - r1.width / 2;
+                let r1r = r1.x + r1.width / 2;
+                let r1t = r1.y - r1.height / 2;
+                let r1b = r1.y + r1.height / 2;
+                let r2l = r2.x - r2.width / 2;
+                let r2r = r2.x + r2.width / 2;
+                let r2t = r2.y - r2.height / 2;
+                let r2b = r2.y + r2.height / 2;
+
+                return !(r2l > r1r ||
+                    r2r < r1l ||
+                    r2t > r1b ||
+                    r2b < r1t);
+            },
+            Updata: function () {
+
+                let movableObjs = this.objects.filter(o => this.isMovableType(o));
+                for (var i = 0; i < movableObjs.length; i++) {
+                    let movableObj = movableObjs[i];
+                    movableObj.pos.x += 3;
+                    let isTouchGround = false;
+                    for (var j = 0; j < this.objects.length; j++) {
+
+
+                        let obj2 = this.objects[j]
+                        if (movableObj.id == obj2.id) continue;
+
+                        if (this.isOverlap_rect(this.getRealBody(movableObj), this.getRealBody(obj2))) {
+                            isTouchGround = true;
+                            this.setIsGround(movableObj);
+                            break;
+                        }
+                    }
+                    if (movableObj.collisionStatus.isground && !isTouchGround) {
+                        console.log('123');
+                        this.setIsFly(movableObj)
+                    }
+
+                }
+                /*
+                for (var i = 0; i < this.objects.length; i++) {
+                    let obj = this.objects[i];
+                    MethodManager.debug(this.getRealBody(obj))
+                }*/
+            }
+
+        }
         let MethodManager = {
+            copyObject: function (obj) {
+                return JSON.parse(JSON.stringify(obj));
+            },
+            gravity: function (velocity) {
+                velocity.y += CharaData.gravitySpeed;
+            },
+            moveing: function (position, velocity) {
+                position.x += velocity.x;
+                position.y += velocity.y;
+            },
+            maxVelocityLimit: function (velocity, limitX, limitY) {
+                if (limitY) {
+                    if (velocity.y > CharaData.velocityLimit.y) {
+                        velocity.y = CharaData.velocityLimit.y;
+                    }
+                }
+                if (limitX) {
+                    if (velocity.x > CharaData.velocityLimit.x) {
+                        velocity.x = CharaData.velocityLimit.x;
+                    }
+                }
+            },
             getImgFactoryObj: function (obj) {
                 return ImgFactory[ObjectData.filter(data => data.type == obj.type)[0].name];
             },
-            imgFactoryDraw: function (ImgFactoryObj, replaceX, replaceY, rotateAngle) {
+            fk: 0,
+            imgFactoryDraw: function (ImgFactoryObj, replacePos, replaceSize, rotateAngle) {
 
-                let x = (replaceX == null) ? ImgFactoryObj.x : replaceX;
-                let y = (replaceY == null) ? ImgFactoryObj.y : replaceY;
+                let x = (replacePos == null) ? ImgFactoryObj.x : replacePos.x;
+                let y = (replacePos == null) ? ImgFactoryObj.y : replacePos.y;
+                let width = (replaceSize == null) ? ImgFactoryObj.width : replaceSize.width;
+                let height = (replaceSize == null) ? ImgFactoryObj.height : replaceSize.height;
 
 
-                if (rotateAngle != null) {
-                    gameCtx.save();
-                    gameCtx.translate(x + ImgFactoryObj.width / 2, y + ImgFactoryObj.height / 2);
-                    gameCtx.rotate(rotateAngle * Math.PI / 180);
-                    gameCtx.globalAlpha = 1;
-                    gameCtx.drawImage(ImgFactoryObj.img, -ImgFactoryObj.width / 2, -ImgFactoryObj.height / 2, ImgFactoryObj.width, ImgFactoryObj.height);
-                    gameCtx.restore();
-                }
-                else {
-                    gameCtx.globalAlpha = 1;
-                    gameCtx.drawImage(ImgFactoryObj.img, ImgFactoryObj.x, ImgFactoryObj.y, ImgFactoryObj.width, ImgFactoryObj.height);
-                }
+                let angle = (rotateAngle == null) ? 0 : rotateAngle;
 
+                /*
+                width -= this.fk;
+                height -= this.fk;
+                this.fk += 0.1;*/
+
+                gameCtx.save();
+                gameCtx.translate(x, y);
+                gameCtx.rotate(angle * Math.PI / 180);
+                gameCtx.globalAlpha = 1;
+                gameCtx.drawImage(ImgFactoryObj.img, -width / 2, -height / 2, width, height);
+                gameCtx.restore();
+
+
+            },
+
+            debug: function (ob) {
+                gameCtx.save();
+                gameCtx.translate(ob.x, ob.y);
+                gameCtx.globalAlpha = 0.5;
+                gameCtx.fillStyle = "#FF0000";
+                gameCtx.fillRect(-ob.width / 2, -ob.height / 2, ob.width, ob.height);
+                gameCtx.restore();
             },
             changeAngle: function (angle) {
                 if (angle >= 360) angle = 0;
@@ -115,8 +239,8 @@
                     MethodManager.imgFactoryDraw(ImgFactory.menu_bg);
                     for (var i = 0; i < ImgFactory.menu_btns.length; i++) {
                         MethodManager.imgFactoryDraw(ImgFactory.menu_btns[i]);
-                        MethodManager.imgFactoryDraw(ImgFactory.menu_circle1, ImgFactory.menu_btns[i].x, ImgFactory.menu_btns[i].y, this.circle1Angle)
-                        MethodManager.imgFactoryDraw(ImgFactory.menu_circle2, ImgFactory.menu_btns[i].x, ImgFactory.menu_btns[i].y, this.circle2Angle)
+                        MethodManager.imgFactoryDraw(ImgFactory.menu_circle1, ImgFactory.menu_btns[i], null, this.circle1Angle)
+                        MethodManager.imgFactoryDraw(ImgFactory.menu_circle2, ImgFactory.menu_btns[i], null, this.circle2Angle)
                     }
                     MethodManager.imgFactoryDraw(ImgFactory.menu_title)
                 }
@@ -131,6 +255,9 @@
                         },
                         end: function () {
                             this.flag = false;
+                        },
+                        isStart: function () {
+                            return this.flag;
                         }
                     },
                     isOpen: false,
@@ -141,9 +268,12 @@
                     this.status.currentStage = StageData.filter(data => data.difficulty == this.status.difficulty && data.no == this.status.no)[0];
 
                 },
-                setDifficulty: function (difficulty) {
+                gameStartInit: function (difficulty) {
                     this.status.difficulty = difficulty;
                     this.setCurrentStage();
+
+                    this.objects.gameStartInit();
+                    this.chara.gameStartInit()
                 },
                 activeOpen: function (flag) {
                     this.status.isOpen = flag;
@@ -152,11 +282,14 @@
                 init: function () {
                     this.background.init();
                     this.objects.init();
+                    this.chara.init()
                     this.frontUI.init();
+
                 },
                 Render: function () {
                     this.background.Render();
                     this.objects.Render();
+                    this.chara.Render()
                     this.frontUI.Render();
 
                 },
@@ -170,17 +303,100 @@
                         MethodManager.imgFactoryDraw(ImgFactory.stage_bg)
                     }
                 },
-                objects: {
+                chara: {
+
+                    position: {},
+                    body: {},
+                    size: {},
+                    collisionStatus: { velocity: { x: 0, y: 0 }, isground: false, canfall: true },
+
+                    currentFrame: 0,
+                    frameTime: 0,
+                    action: "",
+
                     init: function () {
                         this.status = varableManager.stage.status;
+
+
+                    },
+                    gameStartInit: function () {
+                        this.position = this.status.currentStage.startPoint;
+                        this.action = CharaData.ACTION.STAND;
+
+                        this.body = MethodManager.copyObject(CharaData.body);
+                        this.size = MethodManager.copyObject(this.currentImageObj());
+
+                        BodyManager.insertNewBody(BodyManager.TYPE.CHARA, this.position, this.body, this.collisionStatus);
+
+                    },
+                    frameManager: function () {
+                        this.frameTime++;
+                        if (this.frameTime >= CharaData[this.action].frameTime) {
+                            this.frameTime = 0
+                            this.currentFrame++;
+                            if (this.currentFrame >= ImgFactory[this.action].length - 1) {
+                                this.currentFrame = 0;
+                            }
+                        }
+
+                    },
+                    currentImageObj: function () {
+                        return ImgFactory[this.action][this.currentFrame];
+                    },
+                    getBody: function () {
+                        return {
+                            x: CharaData.body.x + this.position.x,
+                            y: CharaData.body.y + this.position.y,
+                            width: CharaData.body.width,
+                            height: CharaData.body.height
+
+                        }
+                    },
+
+                    Render: function () {
+                        if (!this.status.isOpen) return;
+
+
+                        this.frameManager();
+                        if (!this.collisionStatus.isground) {
+                            MethodManager.gravity(this.collisionStatus.velocity);
+                        }
+                        MethodManager.maxVelocityLimit(this.collisionStatus.velocity, true, true);
+                        MethodManager.moveing(this.position, this.collisionStatus.velocity);
+
+
+                        MethodManager.imgFactoryDraw(this.currentImageObj(), this.position, this.size)
+
+
+                    }
+                },
+                objects: {
+                    store: {},
+                    init: function () {
+                        this.status = varableManager.stage.status;
+                    },
+                    gameStartInit: function () {
+                        this.store = MethodManager.copyObject(this.status.currentStage.objects);
+                        for (var i = 0; i < this.store.length; i++) {
+
+                            let imgFactoryObj = MethodManager.getImgFactoryObj(this.store[i]);
+                            this.store[i].imgFactoryObj = { img: imgFactoryObj.img };
+                            this.store[i].size = MethodManager.copyObject(imgFactoryObj);
+                            this.store[i].collisionStatus = { isground: false, canfall: false };
+
+                            let obj = this.store[i];
+
+                            BodyManager.insertNewBody(BodyManager.TYPE.CUBE, obj.position, obj.size, obj.collisionStatus);
+                        }
+
                     },
                     Render: function () {
                         if (!this.status.isOpen) return;
 
-                        for (var i = 0; i < this.status.currentStage.objects.length; i++) {
-                            let obj = this.status.currentStage.objects[i];
-                            let imgFactoryObj = MethodManager.getImgFactoryObj(obj);
-                            MethodManager.imgFactoryDraw(imgFactoryObj, obj.position.x, obj.position.y, obj.angle)
+                        for (var i = 0; i < this.store.length; i++) {
+                            let obj = this.store[i];
+
+                            MethodManager.imgFactoryDraw(obj.imgFactoryObj, obj.position, obj.size, obj.angle)
                         }
                     }
                 },
@@ -213,7 +429,7 @@
                 },
                 menu: {
                     end: function (difficulty) {
-                        varableManager.stage.setDifficulty(difficulty);
+                        varableManager.stage.gameStartInit(difficulty);
                         varableManager.blackscene.setSceneSwitching({
                             previous: varableManager.menu, after: varableManager.stage, callback: function () {
                                 varableManager.stage.status.game.start();
@@ -231,8 +447,8 @@
 
                         let menuBtns = ImgFactory.menu_btns;
                         for (var i = 0; i < menuBtns.length; i++) {
-                            let menuBtnX = menuBtns[i].x + menuBtns[i].width / 2;
-                            let menuBtnY = menuBtns[i].y + menuBtns[i].height / 2;
+                            let menuBtnX = menuBtns[i].x;
+                            let menuBtnY = menuBtns[i].y;
                             let menuBtnRadius = menuBtns[i].width / 2;
 
                             if (MethodManager.isInCircle(xPosition, yPosition, menuBtnX, menuBtnY, menuBtnRadius)) {
@@ -285,7 +501,7 @@
             keydown: {
                 initEvent: function () {
                     window.addEventListener("keydown", function (e) {
-                        if (varableManager.stage.game.flag) {
+                        if (varableManager.stage.status.game.isStart()) {
                             console.log('15fsd6')
                         }
 
@@ -306,9 +522,14 @@
         let Render = function () {
             requestAnimationFrame(Render);
 
+
             varableManager.clear.Render();
+
+
+
             varableManager.menu.Render();
             varableManager.stage.Render();
+            BodyManager.Updata();
             varableManager.blackscene.Render();
 
         }
@@ -421,6 +642,14 @@
             resolve();
         })
     });
+    let loadCharaData = new Promise((resolve) => {
+        fetch('data/charaData.json').then(function (response) {
+            return response.json();
+        }).then(function (JsonObject) {
+            CharaData = JsonObject;
+            resolve();
+        })
+    });
 
     window.onload = function () {
 
@@ -429,7 +658,9 @@
         loadAllImg.then(function () {
             loadStageData.then(function () {
                 loadObjectData.then(function () {
-                    GameManeger();
+                    loadCharaData.then(function () {
+                        GameManager();
+                    })
                 })
             })
         })
